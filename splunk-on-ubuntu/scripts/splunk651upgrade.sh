ba9ed726da036db2e65734aa03ea2187ba1be1f8
Kelvin Yan <kelvinyan85@gmail.com>
2017-02-05 13:45:56 -0800

    new splunk version 6.5.2

--- splunk-on-ubuntu/scripts/splunk651upgrade.sh
+++ splunk-on-ubuntu/scripts/splunk651upgrade.sh
@@ -10,10 +10,12 @@
 (cd /opt/splunk/bin && ./splunk stop)
 
 # download version 6.5.1 to local
-wget -O /opt/splunk-6.5.1-f74036626f0c-Linux-x86_64.tgz 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=6.5.1&product=splunk&filename=splunk-6.5.1-f74036626f0c-Linux-x86_64.tgz&wget=true'
+#wget -O /opt/splunk-6.5.1-f74036626f0c-Linux-x86_64.tgz 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=6.5.1&product=splunk&filename=splunk-6.5.1-f74036626f0c-Linux-x86_64.tgz&wget=true'
+# download version 6.5.2 to local
+wget -O /opt/splunk-6.5.2-67571ef4b87d-Linux-x86_64.tgz 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=6.5.2&product=splunk&filename=splunk-6.5.2-67571ef4b87d-Linux-x86_64.tgz&wget=true'
 
 # unzip the tar file
-tar zxvf /opt/splunk-6.5.1-f74036626f0c-Linux-x86_64.tgz -C /opt/
+tar zxvf /opt/splunk-6.5.2-67571ef4b87d-Linux-x86_64.tgz -C /opt/
 
 # start splunk service
 (cd /opt/splunk/bin && ./splunk start --accept-license --answer-yes)
