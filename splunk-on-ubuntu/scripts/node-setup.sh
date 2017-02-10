a8fecaf797ac58bca08f8b89bfd97ac264ecbdbc
Kelvin Yan <kelvinyan85@gmail.com>
2017-02-10 11:11:32 -0800

    fix

--- splunk-on-ubuntu/scripts/node-setup.sh
+++ splunk-on-ubuntu/scripts/node-setup.sh
@@ -272,7 +272,7 @@ if [ "$NODE_ROLE" == "splunk_cluster_search_head" ] && [ "$MY_IP" == "$SEARCH_HE
   SERVERS_LIST=${SERVERS_LIST::-1}
   sleep 300s
   (cd /opt/splunk/bin && ./splunk bootstrap shcluster-captain -servers_list "${SERVERS_LIST}" -auth "admin:${ADMIN_PASSWD}")
-  echo "Searchhead cluster list: ${SERVERS_LIST}" >> ~\servers_list.txt
+  echo "Searchhead cluster list: ${SERVERS_LIST}" >> ~/servers_list.txt
 fi
 # Save additional iptable changes at the end
 iptables-save > /etc/iptables/rules.v4
